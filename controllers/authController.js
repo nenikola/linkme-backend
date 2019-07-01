63100e9a0aac2173c03617a375b3bbfc9c1db829
nb.nedeljkovic@levi9.com <nb.nedeljkovic@levi9.com>
2019-07-01 23:29:00 +0200

    Added some basic-hardcoded auth.

---
+++ controllers/authController.js
@@ -0,0 +1,4 @@
+exports.isValid = (req, res, next) => {
+  if (req.query.api_key === 'DoXHwSAhJuQErIBfRg1mK3pDsQq2') next();
+  else res.status(401).send('NON-VALID API KEY');
+};
--- routes/items.js
+++ routes/items.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const itemsController = require('../controllers/itemsController.js');
+const authController = require('../controllers/authController.js');
 
 const router = express.Router();
 
-router.get('/', itemsController.getItems);
+router.get('/', authController.isValid, itemsController.getItems);
 
 module.exports = router;
