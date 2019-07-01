0558d0d84a3fc354bca92fcff782d61bc6d52096
nb.nedeljkovic@levi9.com <nb.nedeljkovic@levi9.com>
2019-07-01 23:21:52 +0200

    Server for DJAK and SPORTVISION finished.

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -5,22 +5,57 @@ exports.getItems = (req, res) => {
     if (error) {
       return res.status(404).send();
     }
+    const results = [];
+    const resultPromises = [];
     const data = JSON.parse(response.body);
-    request(data[0].itemUrl, null, (error, response) => {
-      if (error) {
-        return res.status(404).send();
-      }
-      const result = processData(response);
-      res.send(result);
+    data.forEach(element => {
+      resultPromises.push(
+        new Promise((resolve, reject) => {
+          request(element.itemUrl, null, (error, response) => {
+            if (error) {
+              reject();
+            }
+            resolve({ itemUrl: element.itemUrl, response: response });
+          });
+        })
+      );
+    });
+    Promise.all(resultPromises).then(responses => {
+      responses.forEach(element => {
+        results.push(processData(element.itemUrl, element.response));
+      });
+      res.send(results);
     });
   });
 };
 
-function processData(response) {
+function processData(url, response) {
+  if (url.indexOf('djak') != -1) {
+    return processDJAK(response);
+  } else if (url.indexOf('sportvision') != -1) {
+    return processSVISION(response);
+  }
+}
+
+function processDJAK(response) {
   let data = response.body;
   let skripta = data.slice(data.lastIndexOf('<script'), data.length);
   let kod = skripta.slice(skripta.indexOf('>') + 1, skripta.lastIndexOf('</'));
   var Djak = { state: {} };
   eval(kod);
-  return Djak;
+  return {
+    name: Djak.state.product.name,
+    price: Djak.state.product.price,
+    imageUrl: 'https://www.djaksport.com' + Djak.state.product.image.split('&')[0]
+  };
+}
+function processSVISION(response) {
+  let data = response.body;
+  let index1 = data.indexOf('json') + 6;
+  let index2 = data.indexOf(`</script>`, index1);
+  let skripta = data.slice(index1, index2);
+  var OBJ;
+  skripta = 'OBJ=' + skripta;
+  eval(skripta);
+  return { name: OBJ.name, price: OBJ.offers.price + ' din.', imageUrl: OBJ.image };
 }
