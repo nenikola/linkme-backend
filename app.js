9e019941a9c5efa87787445f1fd3df1d4a15aca8
nb.nedeljkovic@levi9.com <nb.nedeljkovic@levi9.com>
2019-07-01 22:22:04 +0200

    Initialized Procfile and changed port.

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const itemsRouter = require('./routes/items.js');
 const app = express();
 app.use('/items', itemsRouter);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log('Server is running...');
 });
