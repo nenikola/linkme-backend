4dd9263cd6ae8830c15ceb935cfa672fd686d51e
nenikola <nenikola4@gmail.com>
2020-03-08 02:21:25 +0100

    git setup

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const itemsRouter = require('./routes/items.js');
+const express = require("express");
+const itemsRouter = require("./routes/items.js");
 
 const app = express();
-app.use('/items', itemsRouter);
+
+app.use("/items", itemsRouter);
 
 app.listen(process.env.PORT, () => {
-  console.log('Server is running...');
+  console.log("Server is running...");
 });
